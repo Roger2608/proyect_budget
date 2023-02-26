e99bb4ee61ee6589033da2070d7fc9be211e6457
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2023-02-25 23:52:22 -0500

    create budget_back

---
+++ budget_back/src/main/java/com/rcyh/budget/BudgetBackApplication.java
@@ -0,0 +1,13 @@
+package com.rcyh.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBackApplication.class, args);
+    }
+
+}
---
+++ budget_back/src/test/java/com/rcyh/budget/BudgetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcyh.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
